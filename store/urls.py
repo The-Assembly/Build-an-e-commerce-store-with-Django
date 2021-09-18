1d0ae821ad86ef19441377262030909c784e13b3
theassembly <32713072+The-Assembly@users.noreply.github.com>
2021-09-18 12:00:58 +0400

    Add files via upload

---
+++ store/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
+]
